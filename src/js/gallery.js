51bd98e8e33b06f9096f999dcbc4622e6abfbe75
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-07-16 17:17:10 +0200

    1
    basic settings plus installation of libraries

---
+++ src/js/gallery.js
@@ -0,0 +1,8 @@
+'use strict';
+/////////biblioteki////////////
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import * as css from 'file.css';
+//////////
