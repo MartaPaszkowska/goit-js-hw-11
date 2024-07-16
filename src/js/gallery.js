7790aeec1d2e9435b95d69641d1d726c762a6b3e
MartaPaszkowska <36098669+MartaPaszkowska@users.noreply.github.com>
2024-07-16 18:18:59 +0200

    style done

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -4,4 +4,14 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-//////////
+//////////błąd//////////////
+iziToast.error({
+  position: 'topRight',
+  icon: 'none',
+  message:
+    'Sorry, there are no images matching your search query. Please try again!',
+  messageColor: 'white',
+  messageSize: '16px',
+  backgroundColor: '#EC3E3E',
+  theme: 'dark',
+});
